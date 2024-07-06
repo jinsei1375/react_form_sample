33dfd15a8f83ba6c514e59839d8684721159d33b
Shinsei <murakami.shinsei.zn@gmail.com>
2024-07-07 00:13:41 +0900

    ヘッダーフッター追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import { Router } from './components/Router';
 
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Router />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>フッター</div>
+    </footer>
+  );
+}
+
+export default Footer;
