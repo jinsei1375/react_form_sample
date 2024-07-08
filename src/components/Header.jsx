b4b89b163227c292c67702bcf382e40747e66ff8
murakami <murakami@kokochie.jp>
2024-07-08 11:10:42 +0900

    リンク横並び

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,8 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header>
-      <Link to="/">Home</Link>
-      <br />
+      <Link to="/">Home</Link> |
       <Link to="form">フォーム</Link>
-      <br />
     </header>
   );
 }
